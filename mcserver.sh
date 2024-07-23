08208a5e29c803702d9bad13b31b4c1412b1b048
AlcyonX <102250632+AlcyonX@users.noreply.github.com>
2024-07-23 19:25:43 +0200

    Update mcserver.sh

--- mcserver.sh
+++ mcserver.sh
@@ -2,28 +2,45 @@
 
 echo "Welcome on MCSERVER.SH from PI TOOLS ! "
 
-if [ -z "$1" ]; then
-  echo "Please give the minecraft version that you want to install. "
-  exit 1
-fi
+echo "Please select a procedure : install, remove or help"
+read choice
 
-function update {
+if ["$choice" == "install"]; then
+
+  echo "Please select a minecraft version : "
+
   cd
   sudo apt update
   sudo apt upgrade
   sudo apt install default-jre
-}
-
-if [ "$1" == "1.8" ]; then
-  echo "Installing..."
-
-  update
-
-  wget https://launcher.mojang.com/v1/objects/b58b2ceb36e01bcd8dbf49c8fb66c55a9f0676cd/server.jar
-
-  echo "success"
+  mkdir Minecraft
+  cd Minecraft
   
+  read choice
+
+  if ["$choice" == "1.8.9"] then
+
+    wget https://launcher.mojang.com/v1/objects/b58b2ceb36e01bcd8dbf49c8fb66c55a9f0676cd/server.jar
+
+    echo "Success ! "
+    
+    else
+    echo "This minecraft version is unsupported..."
+    
+  fi
+
+  elif ["$choice" == "remove"]; then
+
+  cd
+  rm -rf Minecraft
+  echo "Success ! "
+
+  elif ["$choice" == "help"]; then
+
+    echo "Help : "
+    echo "install : Install and run an new mincraft server"  
+    echo "remove : Remove the current minecraft server"
+    
+  else
   
-else
-  echo "This minecraft version is unsupported..."
 fi
